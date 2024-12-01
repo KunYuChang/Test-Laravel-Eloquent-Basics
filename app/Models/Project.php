2394ca55beb27d42e2afb7978cb82790fda76536
KunYuChang <mischiefsub@gmail.com>
2024-12-01 11:57:08 +0800

    change observer setting for github test

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,10 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-use App\Observers\ProjectObserver;
-use Illuminate\Database\Eloquent\Attributes\ObservedBy;
-
-#[ObservedBy([ProjectObserver::class])]
 class Project extends Model
 {
     use HasFactory, SoftDeletes;
